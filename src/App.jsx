28b618518054211b1dfc11bbcec1598393cf31a7
David Ku <dku7@proton.me>
2024-10-27 14:35:24 +0000

    add ability to create new task

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
 import { useState } from "react";
 
 const App = () => {
+  const [todoList, setTodoList] = useState([
+    "display todo items",
+    "make todo items stateful",
+    "create a new todo item",
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  const handNewTaskChange = (event) => setNewTask(event.target.value);
+
+  const handleNewTaskSubmit = (event) => {
+    event.preventDefault();
+    if (!newTask) return;
+
+    setTodoList([newTask, ...todoList]);
+  };
+
   return (
     <>
       <h1>Metodo</h1>
-      <TodoList />
+      <AddNewTask
+        newTask={newTask}
+        onNewTaskChange={handNewTaskChange}
+        onNewTaskSubmit={handleNewTaskSubmit}
+      />
+      <TodoList todoList={todoList} />
     </>
   );
 };
 
-const TodoList = () => {
-  const todoList = ["display todo items", "create a new todo item"];
+const AddNewTask = ({ newTask, onNewTaskChange, onNewTaskSubmit }) => {
+  return (
+    <>
+      <form onSubmit={onNewTaskSubmit}>
+        <label htmlFor="newtask">New task:</label>
+        <input
+          type="text"
+          name="newtask"
+          id="newtask"
+          value={newTask}
+          onChange={onNewTaskChange}
+        />
+        <button type="submit" disabled={!newTask}>
+          Add
+        </button>
+      </form>
+    </>
+  );
+};
 
+const TodoList = ({ todoList }) => {
   return (
     <>
       <ul>
@@ -22,4 +61,5 @@ const TodoList = () => {
     </>
   );
 };
+
 export default App;
