01b511815782821d4e37859450066a3d1ea65bd2
David Ku <dku7@proton.me>
2024-10-27 17:24:46 +0000

    allow adding description and showing on lists

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,20 @@
-const TodoItem = ({ todo, onDoItem, onDeleteItem }) => (
-  <li>
-    {todo.todo}
-    <button onClick={() => onDoItem(todo.id)}>Done</button>
-    <button onClick={() => onDeleteItem(todo.id)}>Delete</button>
-  </li>
-);
+import { useState } from "react";
+
+const TodoItem = ({ todo, onDoItem, onDeleteItem }) => {
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+
+  return (
+    <li>
+      {todo.title}
+
+      <button onClick={() => setIsDescriptionVisible(!isDescriptionVisible)}>
+        Details
+      </button>
+      <button onClick={() => onDoItem(todo.id)}>Done</button>
+      <button onClick={() => onDeleteItem(todo.id)}>Delete</button>
+      {isDescriptionVisible && <p>{todo.description}</p>}
+    </li>
+  );
+};
 
 export default TodoItem;
